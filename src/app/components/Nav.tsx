9dfcfc460c8a27f83f079bc425465d88fad222b4
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-11 15:26:43 -0400

    add nav

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div className="text-gray-300 p-5 shadow shadow-gray-100">
+      <h1 className="text-3xl font-bold">
+        NOTES APP <span>📝</span>
+      </h1>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+/** @format */
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "./components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-black h-screen">
+          <Nav />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
